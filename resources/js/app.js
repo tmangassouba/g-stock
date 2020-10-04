e8a6f618fdf6eb76be75c7c96e616cbdb1e30a01
Tidiane <tidiane.massouba@creaticstudio.com>
2020-10-04 05:58:04 +0000

    buefy install

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,13 @@ import Vue from 'vue';
 import { InertiaApp } from '@inertiajs/inertia-vue';
 import { InertiaForm } from 'laravel-jetstream';
 import PortalVue from 'portal-vue';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
 Vue.use(InertiaApp);
 Vue.use(InertiaForm);
 Vue.use(PortalVue);
+Vue.use(Buefy);
 
 const app = document.getElementById('app');
 
